c376148f14989242e3d4df9d1a485bfd54bbfbd8
hiruni-thaksarani <hirunit@web3genes.com>
2024-10-18 13:54:06 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Allow requests from the frontend URL
+    origin: 'http://localhost:3000', // Allow requests from the frontend URL
     methods: 'GET,POST,PUT,DELETE', // Define allowed methods
     credentials: true, // Enable cookies if needed
   });
